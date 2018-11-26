88473c428a4dc6f05f99b63c79218c9c0856af69
TomaszPlutecki <tplutecki2545@gmail.com>
2018-11-26 13:06:13 +0100

    GraphClasspublic void addNode(int id){         Node a = new Node(id);
            a.setWeight();
    
        }

---
+++ src/com/company/Graph.java
@@ -0,0 +1,33 @@
+package com.company;
+
+
+import java.util.List;
+
+public class Graph {
+
+    Node [][] AdjacencyMatrix = new Node[][];
+
+    public void calcullateVeight (String name){
+
+    }
+
+    public void addNode(int id){
+        Node a = new Node(id);
+
+
+        a.setWeight();
+
+    }
+
+    public void addNode(int id, String name){
+        Node a = new Node(id);
+
+
+    }
+
+    public List<Node>  FindPath(int src, int dst){
+
+
+    }
+
+}
